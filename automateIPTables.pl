b55abb9c3adf80c0317fa94899957ef61b693007
TheGreatestPretender <katschmidt201@gmail.com>
2019-07-11 22:39:28 -0400

    finished :)

--- automateIPTables.pl
+++ automateIPTables.pl
@@ -8,3 +8,20 @@ while(<FILE>){
     chomp $_;
     push(@hold, $_);
 }
+
+while (<>) {
+    $line = $_;
+    chomp $line;
+
+    #if the chomped line has the following string 
+    if ($line =~ m/--log-prefix\"IPT:\"ls*$/i){
+        foreach my$ip(@hold){
+            print("$ip/32 -p -tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT \n");
+            print("$ip/32 -p -udp -m state --state NEW -m udp --dport 22 -j ACCEPT \n");
+        }
+        print "$line\n";
+    }
+    else {
+        print "$line\n";
+    }
+}
